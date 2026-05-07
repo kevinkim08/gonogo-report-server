7e7cf4340b7dbbca8198f3700841f8945ce2db8b
kevinkim08 <wotesaib@gmail.com>
2026-05-07 14:05:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ const __dirname = path.dirname(__filename)
 // =========================================================
 
 app.use(express.json({ limit: "5mb" }))
+app.use(
+    "/fonts",
+    express.static(path.join(__dirname, "fonts"))
+)
 
 app.use(
     cors({
