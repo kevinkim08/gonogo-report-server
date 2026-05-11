94bfb6c9b1c131e31ad8f79f1c4fcdb76608f3ca
kevinkim08 <wotesaib@gmail.com>
2026-05-11 18:50:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ import { createClient } from "@supabase/supabase-js"
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const REPORT_PRICE = "49.00"
+const REPORT_PRICE = "1.00"
 const REPORT_CURRENCY = "USD"
 
 app.set("trust proxy", true)
